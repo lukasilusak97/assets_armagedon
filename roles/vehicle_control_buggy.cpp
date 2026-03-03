56ae2c7568be6f8686d74e81485779288f90500f
Gpu_Fox <rastislav.skopinsky3@gmail.com>
2026-03-03 19:18:10 +0100

    controls_new

--- roles/vehicle_control_buggy.cpp
+++ roles/vehicle_control_buggy.cpp
@@ -1,11 +1,20 @@
+#include "audio/audio.h"
+#include "input/input_map.h"
+#include "physics/vehicle_commands.h"
+#include "physics/vehicle_data.h"
+#include "physics/vehicle_state.h"
+#include "roles/sound_manager_role.h"
+#include "world/game_interface.h"
 #include "world/plugin_role_tools.h"
-#include "world/role_props.h"
-#include "world/world.h"
+#include "world/sim_module.h"
+#include "utils/config.h"
 
 using namespace std;
-using namespace ella;
 using namespace ella::utils;
 using namespace ella::world;
+using namespace ella::input;
+using namespace ella::physics;
+using namespace ella::roles;
 
 namespace ella::roles::vehicle_control_buggy 
 {
@@ -14,11 +23,48 @@ struct Data : public Base
 };
 
 IDKEY(vehicle_control_buggy);
-VKEY(example_var);
+
+VKEY(vehicle_link);
+VKEY(vehicle_role);
+
+auto OnAction(RoleRef r)
+{
+    auto const & c = GetActionContext();
+    auto soundManagerRole = r->GetOrCreateModule(roles::sound_manager::id);
+    // auto d = r->GetCore<Data>();
+    const auto vehicle = r->GetBase<Role>(vehicle_role);
+    if (!vehicle)
+    {
+        LogLI("No vehicle linked to vehicle control role");
+        return;
+    }
+    LogW("Vehicle input key: {} , value: {}", c->key, c->GetValue());
+    if (c->key == "forward" ) 
+    {
+        vehicle->Set("throttle", c->IsEnabled() ? 1.0 : 0.0);
+        vehicle->Set("engine_target_gear", 2);
+    }
+    if (c->key == "reverse"  ) 
+    {
+        vehicle->Set("throttle", c->IsEnabled() ? 1.0 : 0.0);
+        
+        vehicle->Set("engine_target_gear", 0);
+    }
+    
+        
+        
+    
+}
 
 auto Ready(RoleRef r)
 {
-    LogD("Example var: {}", r->GetInt(example_var));
+    auto imp = r->GetRole("user_input")->GetBase<InputMap>();
+    imp->OnAnyAction()->AddIfSingle(r, [r]()
+    {
+        OnAction(r);
+    });
+    
+ 
 }
 
 auto RegRole()
@@ -27,8 +73,9 @@ auto RegRole()
     p.SetCore<Data>();
     p.Category("examples");
     p.AddGroup(RoleGroups::step);
+    p.AddRoleCachedLink(vehicle_link, vehicle_role).AddFilter("vehicle_base");
 
-    p.AddInt(example_var);
+    
 
     p.ready = Ready;
     return true;
